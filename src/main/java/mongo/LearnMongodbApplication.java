94a09d2133025bded9d33e6757df6377c2cffd24
yurabh <yurabh1990@gmail.com>
2022-11-12 21:40:30 +0200

    Add swagger to progect.

--- src/main/java/mongo/LearnMongodbApplication.java
+++ src/main/java/mongo/LearnMongodbApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class})
+@EnableSwagger2
 public class LearnMongodbApplication {
     public static void main(String[] args) {
         SpringApplication.run(LearnMongodbApplication.class, args);
